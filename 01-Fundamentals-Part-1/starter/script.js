87056eceec850b3de3e8248b705f4946b285f3b6
oskarziembrowicz <rakson2002@gmail.com>
2023-07-07 12:20:01 +0200

    Learning about variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,31 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is fun');
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// console.log('Jonas');
+// console.log(23)
+
+let country = "Poland";
+let continent = "Europe";
+let population = 34
+
+// console.log(country + ", " + continent + ", " + population);
+
+// let javaIsFun = true;
+
+// console.log(typeof true);
+// console.log(typeof javaIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+// javaIsFun = "YES";
+// console.log(typeof javaIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let isIsland = false;
+let language;
+
---
+++ Assignments/script.js
@@ -0,0 +1,10 @@
+let country = "Poland";
+let continent = "Europe";
+let population = 34;
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
