973f30368ae4e6a4327c826cb904b897fa748525
oskarziembrowicz <rakson2002@gmail.com>
2023-07-08 15:23:49 +0200

    Learning the ternary operator

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -161,27 +161,36 @@
 // }
 
 
-const day = 'monday';
+// const day = 'monday';
 
-switch (day) {
-    case 'monday':
-        console.log('Plan course structure');
-        console.log('Go to coding meetup');
-        break;
-    case 'tuesday':
-        console.log('Prepare theory videos');
-        break;
-    case 'wednesday':
-    case 'thursday':
-        console.log('Write code examples');
-        break;
-    case 'friday':
-        console.log('Record videos');
-        break;
-    case 'saturday':
-    case 'sunday':
-        console.log('Enjoy the weekend');
-        break;
-    default:
-        console.log('Not a valid day!');
-}
+// switch (day) {
+//     case 'monday':
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup');
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('Write code examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+// }
+
+
+const age = 20;
+age >= 18 ? console.log("I like to dring wine 🍷") : console.log("I like to drink milk 🥛");
+
+const drink = age >= 18 ? 'wine 🍷' : 'milk 🥛';
+console.log(drink);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'milk 🥛'}`);
