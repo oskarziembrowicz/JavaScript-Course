719234f2f4fec1abfdb03eb5a306b968db5de981
oskarziembrowicz <rakson2002@gmail.com>
2023-07-13 18:51:33 +0200

    Manipulating styles of objects

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -21,10 +21,19 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(typeof guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "🚫 No number!";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉Correct number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
@@ -34,6 +43,8 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".message").textContent = "😿 You lost the game!";
       document.querySelector(".score").textContent = 0;
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
