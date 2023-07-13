d07fc1a782af81bc1cf630ed57a1f635a8765207
oskarziembrowicz <rakson2002@gmail.com>
2023-07-13 17:59:22 +0200

    Learning to intereact with the DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct number!🎉";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 10;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
