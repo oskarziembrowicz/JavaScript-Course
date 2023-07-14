66e060a3eef8307ac6a91a16bb3c1efd5ae44ad6
oskarziembrowicz <rakson2002@gmail.com>
2023-07-14 18:20:26 +0200

    Adding Esc key handling

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -20,5 +20,10 @@ for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
