9cbbaef27b08a93bf7f5bc60664a80cae634dabc
oskarziembrowicz <rakson2002@gmail.com>
2023-07-15 13:39:44 +0200

    Starting Pig-Game project and setting up starting screen

---
+++ 07-Pig-Game/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Selecting elements
+const score0Elelent = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Elelent.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
