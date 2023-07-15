e56089684af78862cf129a7557e96bee2abbbdf7
oskarziembrowicz <rakson2002@gmail.com>
2023-07-15 15:07:32 +0200

    Implementing switching active player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Elelent = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
 const currentScore0Element = document.getElementById('current--0');
@@ -14,7 +16,9 @@ score0Elelent.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling the dice functionality
 buttonRoll.addEventListener('click', function () {
@@ -29,8 +33,19 @@ buttonRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    currentScore0Element.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
+
+// buttonHold.addEventListener("click", function () {
+
+// });
