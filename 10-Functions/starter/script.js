70fbf477b894abeb3380fb01c40c267024a458b5
oskarziembrowicz <rakson2002@gmail.com>
2023-07-17 16:00:54 +0200

    Learning about functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+/*
+
 // HOW PASSING ARGUMENTS WORKS
 
 const flight = 'LH234';
@@ -33,3 +35,34 @@ const newPassport = function (person) {
 
 newPassport(oscar);
 checkIn(flight, oscar);
+
+*/
+
+// FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('✋');
+};
+document.body.addEventListener('click', high5);
+['Oscar', 'Adam', 'Martha'].forEach(high5);
