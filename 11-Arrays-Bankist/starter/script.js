8677c99fc6f1a0380cf90b638097cc2acc39bde3
oskarziembrowicz <rakson2002@gmail.com>
2023-07-18 13:32:33 +0200

    Learning about forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -116,7 +116,7 @@ console.log([...arr, ...arr2]);
 // Join
 console.log('Join:');
 console.log(letters.join(' - '));
-*/
+
 // At
 const arr = [23, 11, 64];
 console.log(arr[0]);
@@ -128,3 +128,27 @@ console.log(arr.at(-1));
 
 console.log('oscar'.at(-1));
 console.log('oscar'[1]);
+
+*/
+
+//FOR EACH
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOR EACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
