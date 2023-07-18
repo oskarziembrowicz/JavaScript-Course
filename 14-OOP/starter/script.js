278c12b90b66f9c5b240de932b46f5a77a5f4366
oskarziembrowicz <rakson2002@gmail.com>
2023-07-18 16:13:05 +0200

    Prototypes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -27,3 +27,27 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(oscar instanceof Person);
+
+// PROTOTYPES
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+oscar.calcAge();
+matilda.calcAge();
+
+console.log(oscar.__proto__);
+console.log(oscar.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(oscar));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// Person.prototype is a prototype of linked objects, IT IS NOT the prototype of Person
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(oscar.species, matilda.species);
+
+console.log(oscar.hasOwnProperty('firstName'));
+console.log(oscar.hasOwnProperty('species'));
