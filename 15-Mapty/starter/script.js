794d6342aada54a0850d015e5ce68c6dab2c7bb5
oskarziembrowicz <rakson2002@gmail.com>
2023-08-15 16:27:45 +0200

    Saving workouts in localStorage

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -80,7 +80,13 @@ class App {
   #mapZoom = 13;
 
   constructor() {
+    // Get users position
     this._getPosition();
+
+    // Get data from local storage
+    this._getLocalStorage();
+
+    // Attatch event handlers
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
     containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
@@ -114,6 +120,10 @@ class App {
 
     // Handling clicks on map
     this.#map.on('click', this._showForm.bind(this));
+
+    this.#workouts.forEach(workout => {
+      this._renderWorkoutMarker(workout);
+    });
   }
 
   _showForm(mapE) {
@@ -198,6 +208,9 @@ class App {
 
     // Hide form + clear input fields
     this._hideForm();
+
+    // Set localStorage to all workouts
+    this._setLocalStorage();
   }
 
   _renderWorkoutMarker(workout) {
@@ -290,7 +303,23 @@ class App {
     });
 
     // Using the publick interface
-    workout.click();
+    // workout.click();
+  }
+
+  _setLocalStorage() {
+    localStorage.setItem('workouts', JSON.stringify(this.#workouts));
+  }
+
+  _getLocalStorage() {
+    const data = JSON.parse(localStorage.getItem('workouts'));
+    console.log(data);
+
+    if (!data) return;
+
+    this.#workouts = data;
+    this.#workouts.forEach(workout => {
+      this._renderWorkout(workout);
+    });
   }
 }
 
