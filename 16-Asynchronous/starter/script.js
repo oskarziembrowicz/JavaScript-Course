4a5d457e13ffc7374f7c0a8566963f8cb763b513
oskarziembrowicz <rakson2002@gmail.com>
2023-10-12 18:27:39 +0200

    Chaining promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -129,9 +129,18 @@ setTimeout(() => {
 // }
 
 function getCountryData(country) {
+  // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 }
 
 getCountryData('poland');
