e8a475708e7f34d3d296d54a7c4c99127e2a08a6
oskarziembrowicz <rakson2002@gmail.com>
2023-10-12 18:19:22 +0200

    Using fetch() and promises to simplyfy callbacks

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -62,6 +62,7 @@ function renderCountry(data, calssName = '') {
   countriesContainer.style.opacity = 1;
 }
 
+/*
 function getCountryAndNeighbour(country) {
   // AJAX call country 1
   const request = new XMLHttpRequest();
@@ -108,3 +109,29 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+
+*/
+
+// const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+// request.send();
+
+// function getCountryData(country) {
+//   fetch(`https://restcountries.com/v3.1/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// }
+
+function getCountryData(country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+}
+
+getCountryData('poland');
