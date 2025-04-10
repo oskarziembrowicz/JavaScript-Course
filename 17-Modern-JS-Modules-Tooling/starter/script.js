42de2b6d70b6a7419bd962414135492d8173c1dc
oskarziembrowicz <rakson2002@gmail.com>
2025-04-10 18:25:02 +0200

    Installing modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,6 @@
+////////////////////////////////////
+// IMPORTING MODULES
+/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
@@ -15,6 +18,7 @@ add('apple', 7);
 add('pizza', 2);
 add('bread', 5);
 console.log(cart);
+*/
 
 /////////////////////////////////////////
 // TOP LEVEL AWAIT
@@ -41,3 +45,19 @@ const getLastPost = async function () {
 const lastPost = await getLastPost();
 console.log(lastPost);
 */
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+console.log(stateDeepClone);
